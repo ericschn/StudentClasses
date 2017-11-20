306534659808473c9607301dbee8ad3e12614c79
ericschn <eschneider327@gmail.com>
2017-11-20 10:26:38 -0600

    initial commit

---
+++ src/org/launchcode/Student.java
@@ -0,0 +1,66 @@
+package org.launchcode;
+
+public class Student {
+
+    private static int nextStudentId = 1;
+    private String name;
+    private int studentId;
+    private int numberOfCredits;
+    private double gpa;
+
+    public Student(String name, int studentId,
+                   int numberOfCredits, double gpa) {
+        this.name = name;
+        this.studentId = studentId;
+        this.numberOfCredits = numberOfCredits;
+        this.gpa = gpa;
+    }
+
+    public Student(String name, int studentId) {
+        this(name, studentId, 0, 0);
+    }
+
+    public Student(String name) {
+        this(name, nextStudentId);
+        nextStudentId++;
+    }
+
+    public void addGrade(int courseCredits, double grade) {
+        // Update the appropriate fields: numberOfCredits, gpa
+    }
+
+    public String getGradeLevel() {
+
+        return "not yet implemented";
+
+        // Determine the grade level of the student based on numberOfCredits
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+
+    public int getNumberOfCredits() {
+        return numberOfCredits;
+    }
+
+    public void setNumberOfCredits(int numberOfCredits) {
+        this.numberOfCredits = numberOfCredits;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
